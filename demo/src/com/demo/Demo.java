af413e288351c02438572313765861947f936a3f
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-04-14 10:28:32 +0530

    demo

--- demo/src/com/demo/Demo.java
+++ demo/src/com/demo/Demo.java
@@ -1,10 +1,12 @@
 package com.demo;
 
 public class Demo {
+	
+	int bb=150;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hii");
+		//System.out.println("hii");
 		Demo d=new Demo();
 		d.variable();
 	}
@@ -12,6 +14,9 @@ public class Demo {
 		// TODO Auto-generated method stub
 		int a=25;
 		System.out.println(a);
+		
+		System.out.println(bb);
+		
 		
 	}
 
