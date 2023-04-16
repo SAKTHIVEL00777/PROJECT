6b59864d2f713653da0f84a2bf47187bbee4dc26
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-04-16 16:10:43 +0530

    demo

--- demo/src/com/demo/Demo.java
+++ demo/src/com/demo/Demo.java
@@ -6,7 +6,8 @@ public class Demo {
 	static String village="sengai";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.out.println("hii");
+
+		System.out.println("hii");
 		Demo d=new Demo();
 		d.variable();
 	}
