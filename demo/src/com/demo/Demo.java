4a8c68a96a77f9b9d9ef9c8c5e71d6041382f6a7
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-05-28 12:46:49 +0530

    demo

--- demo/src/com/demo/Demo.java
+++ demo/src/com/demo/Demo.java
@@ -5,31 +5,60 @@ public class Demo {
 	int bb=150;
 	static String village="sengai";
 	public static void main(String[] args) {
-		/// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
 		System.out.println("HELLO");
 		Demo d=new Demo();
-		d.variable();
+		//d.variable();
+		d.viwol();
 	}
+	private void viwol() {
+		// TODO Auto-generated method stub
+		//a=@, e=$, i=&, o=%, u=#
+		String name="Hello Qantler";
+		String word="";
+		char ch[]=name.toCharArray();
+		for(int i=0;i<name.length();i++){
+			if(name.charAt(i)=='a'){
+				word+='@';
+			}
+			else if(name.charAt(i)=='e'){
+				word+='$';
+			}
+			else if(name.charAt(i)=='i'){
+				word+='&';
+			}
+			else if(name.charAt(i)=='o'){
+				word+='%';
+			}
+			else if(name.charAt(i)=='u'){
+				word+='#';
+			}
+			else{
+				word+=ch[i];
+			}
+		}
+		System.out.println(word);
+		}
 	private void variable() {
-		/// TODO Auto-generated method stub
-	       int a=25;
-	       String name="ABD";
-	       Boolean boo=true;
-	       Float f=523.5f;
-	       Double d=9856d;
-	       long set=987654095;
-	       Character let='c';
-
-	      System.out.println(a);
-	      System.out.println(bb);
-	      System.out.println(village);
-              System.out.println(name);
-              System.out.println(boo);
-              System.out.println(f);
-              System.out.println(d);
-              System.out.println(set);
-              System.out.println(let);
+		// TODO Auto-generated method stub
+		int a=25;
+	    String name="ABD";
+	    Boolean boo=true;
+	    Float f=523.5f;
+	    Double d=9856d;
+	    long set=987654093;
+	    Character let='c';
+	    
+		System.out.println(a);
+		System.out.println(bb);
+		System.out.println(village);
+        System.out.println(name);
+        System.out.println(boo);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(set);
+        System.out.println(let);
 
 		
 		
