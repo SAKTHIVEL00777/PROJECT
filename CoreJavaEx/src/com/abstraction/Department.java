a79a3acbf88bbfaff813774a3721084be46eb324
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-01-29 12:29:48 +0530

    abstract keyword using to the classname & methods

---
+++ CoreJavaEx/src/com/abstraction/Department.java
@@ -0,0 +1,13 @@
+package com.abstraction;
+
+public abstract class Department {  //-->Abstract class
+	
+	public void dept(){
+		System.out.println("ALL DEPARTMENTS:");
+	}
+	
+	public abstract void cs();   //--->method declaration-Abstract Method
+	
+	public abstract void bca();  //--->method declaration-Abstract Method
+
+}
