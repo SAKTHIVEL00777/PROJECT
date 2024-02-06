f490406dd5f5bd8735953b2f1f1e7743d4d69635
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-02-06 17:35:54 +0530

    method overriding

---
+++ CoreJavaEx/src/com/polymorphism/RedColor.java
@@ -0,0 +1,11 @@
+package com.polymorphism;
+
+public class RedColor {
+
+	void color(){
+		System.out.println("ALL COLORS ACCEPTED");
+	}
+	void red(){
+		System.out.println("APPLE COLOR IS RED");
+	}
+}
