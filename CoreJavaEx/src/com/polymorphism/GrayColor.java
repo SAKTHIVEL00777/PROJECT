668ac5c81cdbf025b211ddd730c648e319d14ba8
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-02-07 18:06:56 +0530

    method overriding

---
+++ CoreJavaEx/src/com/polymorphism/GrayColor.java
@@ -0,0 +1,11 @@
+package com.polymorphism;
+
+public class GrayColor extends RedColor {
+
+	void color(){
+		System.out.println("ALL COLORS ACCEPTED");
+	}
+	void gray(){
+		System.out.println("APSARA PENCIL");
+	}
+}
