536c48e8d1d93cd8932e3eb5f1698f58d1414c22
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-02-08 17:55:21 +0530

    method overriding

---
+++ CoreJavaEx/src/com/polymorphism/BlueColor.java
@@ -0,0 +1,11 @@
+package com.polymorphism;
+
+public class BlueColor extends GrayColor {
+
+	void color(){
+		System.out.println("ALL COLORS ACCEPTED");
+	}
+	void blue(){
+		System.out.println("SHOP IS BLUE COLOR");
+	}
+}
