00ba699208f9c28fe9a76d715796757bac1cfe2f
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-02-13 16:37:47 +0530

    access modifier default

---
+++ CoreJavaEx/src/com/modifierdefault/DefaultModifierOne.java
@@ -0,0 +1,14 @@
+package com.modifierdefault;
+
+public class DefaultModifierOne {
+
+	public static void main(String[] args) {
+	
+		DefaultModifier dm=new DefaultModifier();
+		
+		System.out.println(dm.staffName);
+		
+		dm.staff(4, "NAMAKAL");
+	}
+
+}
