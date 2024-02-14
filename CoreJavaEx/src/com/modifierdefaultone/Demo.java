41156589049f3289903e33f04e54175d814eeebe
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-02-14 17:09:07 +0530

    access modifier defaultOne

---
+++ CoreJavaEx/src/com/modifierdefaultone/Demo.java
@@ -0,0 +1,14 @@
+package com.modifierdefaultone;
+
+import com.modifierdefault.DefaultModifier;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		DefaultModifier dm=new DefaultModifier();
+		
+		//dm.staff(10,"Trichy");--not possible
+	}
+
+}
