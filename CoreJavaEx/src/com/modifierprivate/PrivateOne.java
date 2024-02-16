96ddb1931e5b125c3267c8bfa2c4ae609fcdbe3f
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-02-16 18:49:02 +0530

    access modifier private

---
+++ CoreJavaEx/src/com/modifierprivate/PrivateOne.java
@@ -0,0 +1,13 @@
+package com.modifierprivate;
+
+public class PrivateOne {
+
+	public static void main(String[] args) {
+
+		PrivateModifier pm=new PrivateModifier();
+		
+		//System.out.println(pm.empId);  --->Not Available
+	    //pm.empName();                 --->Not Available
+	}
+
+}
