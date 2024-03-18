63770fe019e693f953f285806586a83e6c723c3b
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-03-18 12:37:27 +0530

    Set

---
+++ CoreJavaEx/src/com/collection/SetEg.java
@@ -0,0 +1,27 @@
+package com.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetEg {
+
+	public static void main(String[] args) {
+
+
+		Set<String> set=new HashSet<>();
+		set.add("MURALI");
+		set.add("SIVA");
+		set.add("DHIVAGAR");
+		set.add(null);
+		set.add("RAM");
+		set.add(null);
+		set.add("JEEVA");
+		set.add("MARI");
+		set.add("RAM");
+		
+		System.out.println("SET LIST:"+ set);
+		
+		
+	}
+
+}
