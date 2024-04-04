d2356e28d5abb64c52ead5a79fa3d6a9634630a8
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-04-04 10:34:02 +0530

    demo doget call

---
+++ servletweb/src/main/java/com/techymeet/servletweb/controller/Demo.java
@@ -0,0 +1,48 @@
+package com.techymeet.servletweb.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo
+ */
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Demo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		/*
+		 * response.getWriter().append("Served at: ").append(request.getContextPath());
+		 * response.getWriter().append(" This is Web Technology");
+		 * System.out.println("doGet Call.......");
+		 */
+		RequestDispatcher rd=request.getRequestDispatcher("demo.html");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
