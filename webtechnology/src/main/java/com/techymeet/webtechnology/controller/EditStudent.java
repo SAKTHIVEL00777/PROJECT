d4939c14f594ce379f576669ed040ff2c722da10
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-04-25 11:22:39 +0530

    edit student

---
+++ webtechnology/src/main/java/com/techymeet/webtechnology/controller/EditStudent.java
@@ -0,0 +1,48 @@
+package com.techymeet.webtechnology.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EditStudent
+ */
+@WebServlet("/editStudent")
+public class EditStudent extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditStudent() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String studId=request.getParameter("studId");
+		String studName=request.getParameter("studName");
+		String city=request.getParameter("city");
+
+		response.getWriter().println(studId);
+		response.getWriter().println(studName);
+		response.getWriter().println(city);
+
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
