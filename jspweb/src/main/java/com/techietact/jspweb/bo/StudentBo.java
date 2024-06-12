2538807c2806fa8d96b5ab53da4b12f5ab689f36
SAKTHIVEL00777 <sakthi040797@gmail.com>
2024-06-12 15:28:33 +0530

    studentbo

---
+++ jspweb/src/main/java/com/techietact/jspweb/bo/StudentBo.java
@@ -0,0 +1,29 @@
+package com.techietact.jspweb.bo;
+
+public class StudentBo {
+
+	private int studId;
+	private String studName;
+	private String email;
+	
+	public int getStudId() {
+		return studId;
+	}
+	public void setStudId(int studId) {
+		this.studId = studId;
+	}
+	public String getStudName() {
+		return studName;
+	}
+	public void setStudName(String studName) {
+		this.studName = studName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
